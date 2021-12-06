cabc0d62ebcda93b7dc60a1fc4c8dbc392189a25
lintangsutawika <lintang@sutawika.com>
2021-12-06 23:57:37 +0700

    test

--- multilingual_t0/d4_train.sh
+++ multilingual_t0/d4_train.sh
@@ -9,4 +9,5 @@ python3 ${T5X_DIR}/t5x/train.py \
   --tfds_data_dir=${TFDS_DATA_DIR} \
   --gin_file="mt0_xxl_finetune.gin" \
   --gin.MODEL_DIR="'${MODEL_DIR}'" \
+  --gin.train.infer_eval_dataset_cfg=None \
   --gin.MIXTURE_OR_TASK_NAME="'d4_train'"
