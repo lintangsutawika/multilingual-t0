f40f79581867dcf2b48eaa57fe4f507d51e16327
lintangsutawika <lintang@sutawika.com>
2021-12-21 15:44:33 +0700

    added gin path

--- multilingual_t0/scripts/mt0_xl_t0pp.sh
+++ multilingual_t0/scripts/mt0_xl_t0pp.sh
@@ -10,7 +10,7 @@ LOGS_PATH="/home/lintang/logs"
 mkdir -p $LOGS_PATH
 
 python3 ${T5X_DIR}/t5x/train.py \
-  --gin_search_paths=${PROJECT_DIR}
+  --gin_search_paths=${PROJECT_DIR} \
   --gin_file="mt0_xl_finetune.gin" \
   --gin.MODEL_DIR="'${MODEL_DIR}'" \
   --gin.train.infer_eval_dataset_cfg=None \
