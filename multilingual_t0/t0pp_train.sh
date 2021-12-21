d6641cacf85b5c42ffd6554c34ff0d46f3295131
lintangsutawika <lintang@sutawika.com>
2021-12-21 09:47:39 +0700

    continue training

--- multilingual_t0/t0pp_train.sh
+++ multilingual_t0/t0pp_train.sh
@@ -16,4 +16,6 @@ python3 ${T5X_DIR}/t5x/train.py \
   --gin.train.train_eval_dataset_cfg=None \
   --gin.trainer.Trainer.num_microbatches=16 \
   --gin.MIXTURE_OR_TASK_NAME="'t0_plus_plus_train'" \
+  --gin.INITIAL_CHECKPOINT_PATH="'gs://bigscience-t5x/multilingual_t0/t0pp_train/checkpoint_1025000'" \
+  --gin.TRAIN_STEPS=1037000 \
   2>&1 | tee $LOGS_PATH/train_$EXPERIMENT_NAME.txt
