e462a01ae3a1a3c77754a76f5e4002cd8d478e04
yongzx <zheng_xin_yong@brown.edu>
2022-02-03 22:48:35 -0500

    mC4_download

---
+++ mC4_download/prepare_dataset.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Request half an hour of runtime:
+#SBATCH --time=1-23:59:00
+
+# Ask for the GPU partition and 1 GPU
+#SBATCH --partition=3090-gcondo --gres=gpu:1
+
+# Default resources are 1 core with 2.8GB of memory.
+#SBATCH --ntasks=2
+
+# Use more memory (10GB) (CPU RAM):
+
+# Specify a job name:
+#SBATCH -J exp-001-prepare_dataset
+
+# Specify an output file
+#SBATCH -o /users/zyong2/data/zyong2/mt0/logs/log-001/prepare_dataset.out
+#SBATCH -e /users/zyong2/data/zyong2/mt0/logs/log-001/prepare_dataset.err
+
+
+# load python
+module load python/3.7.4
+
+# set up current directory path
+CURRENT_DIR="/users/zyong2/data/zyong2/mt0/data/external/mt0/mC4_download"
+
+# set up virtual environment and install necessary pip packages
+python3 -m venv "${CURRENT_DIR}/env_prepare_dataset"
+source "${CURRENT_DIR}/env_prepare_dataset/bin/activate"
+pip3 install --upgrade pip
+pip3 install -r "${CURRENT_DIR}/requirements.txt"
+
+# download data
+DATA_DIR="${CURRENT_DIR}/data"
+mkdir -p $DATA_DIR
+python3 prepare_dataset.py --base_dir $DATA_DIR --num_process 8
